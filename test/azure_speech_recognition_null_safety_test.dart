2c753765979f9dcc75bd04e94c81f969a98caf79
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2023-03-04 18:45:39 -0500

    fix: minimal fix

--- test/azure_speech_recognition_null_safety_test.dart
+++ test/azure_speech_recognition_null_safety_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:azure_speech_recognition_null_safety/azure_speech_recognition_null_safety.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('azure_speech_recognition');
